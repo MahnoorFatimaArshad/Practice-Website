b361de646a4ebf1a188caad56683cd505e44a950
mahnoorfatimaahmad <mahnoor.fatima5654@gmail.com>
2025-05-27 13:23:55 -0700

    Fix Netlify build configuration

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    unoptimized: true
+  },
+  swcMinify: true
+};
+
+module.exports = nextConfig; 
