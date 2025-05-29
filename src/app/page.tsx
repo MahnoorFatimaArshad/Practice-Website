7bd38406e49a78080e048f4d573e764a3c4c4cf0
mahnoorfatimaahmad <mahnoor.fatima5654@gmail.com>
2025-05-29 03:45:14 -0700

    update code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,28 @@
-// import React from 'react'
-// import Hero from './Components/Hero'
-// import Companies from './Components/Companies'
-// import Discription from './Components/Discription'
-// import PriceSection from './Components/Pricing'
-// import UsersSay from './Components/UserSay'
-// import TodayOffer from './Components/TodayOffer'
-// import Footer from './Components/Footer'
+import React from 'react'
+import Hero from './Components/Hero'
+import Companies from './Components/Companies'
+import Discription from './Components/Discription'
+import PriceSection from './Components/Pricing'
+import UsersSay from './Components/UserSay'
+import TodayOffer from './Components/TodayOffer'
+import Footer from './Components/Footer'
 
-// function Home() {
-//   return (
-//     <main className="min-h-screen w-full bg-white overflow-x-hidden">
-//       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-//         <Hero />
-//         <div className="space-y-24 py-16">
-//           <Companies />
-//           <Discription />
-//           <PriceSection />
-//           <UsersSay />
-//           <TodayOffer />
-//         </div>
-//       </div>
-//       <Footer />
-//     </main>
-//   )
-// }
-
-// export default Home
-
-
-
-
-export default function Home() {
+function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-100">
-<h1 className="text-4xl font-bold text-blue-600 bg-red-200 p-4 rounded-xl">
-
-        Tailwind is Working 🚀</h1>
+    <main className="min-h-screen w-full bg-white overflow-x-hidden">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <Hero />
+        <div className="space-y-24 py-16">
+          <Companies />
+          <Discription />
+          <PriceSection />
+          <UsersSay />
+          <TodayOffer />
+        </div>
+      </div>
+      <Footer />
     </main>
   )
 }
 
+export default Home;
